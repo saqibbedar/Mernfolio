1e08da117bf63f91b88757cca43dffbf5f024dc9
saqibbedar <chandio.saqib.m@gmail.com>
2024-08-24 15:37:04 +0500

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const ConnectToDatabase = async (mongoDB_URL) => {
     await mongoose.connect(mongoDB_URL).then(()=>console.log("Connected to the database"));
