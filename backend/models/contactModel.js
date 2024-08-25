62344c0ec5e21d52e24653b493a3187250a377fd
saqibbedar <chandio.saqib.m@gmail.com>
2024-08-25 15:22:01 +0500

    update contact model

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -4,7 +4,7 @@ const contactSchema = new mongoose.Schema({
     name : {type : String, required : true},
     email : {type : String, required : true},
     message : {type : String, required : true}
-})
+}, { timestamps: true });
 
 const contactModel = mongoose.models.contact || mongoose.model("contact", contactSchema);
 
